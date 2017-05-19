0b16ac2ddf204fb6c17a2dbe2dc4b9dfa72d0f12
Peter Meyer <rvg156@alumni.ku.dk>
2017-05-19 10:07:40 +0200

    added visualisation of gesturepad

--- MC-UI/src/main/java/com/physicaloid/mcui/MCUIActivity.java
+++ MC-UI/src/main/java/com/physicaloid/mcui/MCUIActivity.java
@@ -12,14 +12,19 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Matrix;
 import android.graphics.Paint;
+import android.graphics.Point;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.Handler;
+import android.util.DisplayMetrics;
 import android.util.Log;
+import android.view.Display;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -34,6 +39,8 @@ import com.physicaloid.lib.usb.driver.uart.ReadLisener;
 import static com.physicaloid.mcui.R.id.imageView;
 
 public class MCUIActivity extends Activity {
+
+
     private static final String TAG = MCUIActivity.class.getSimpleName();
     /*
      * !!! You need to import PhysicaloidLibrary. !!!
@@ -51,9 +58,13 @@ public class MCUIActivity extends Activity {
     EditText etWrite;
     TextView tvRead;
     ImageView imageView;
+    FloatPoint bt0, bt1, bt2, bt3;
+
+
     private static Context context;
 
     Physicaloid mPhysicaloid;
+    Bitmap scaledBitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -69,82 +80,54 @@ public class MCUIActivity extends Activity {
         tvRead  = (TextView) findViewById(R.id.tvRead);
         imageView  = (ImageView) findViewById(R.id.imageView);
 
-        setEnabledUi(false);
+        DisplayMetrics displayMetrics = context.getResources().getDisplayMetrics();
+        float w = displayMetrics.widthPixels;
+        float h = w;
 
+        setEnabledUi(false);
         mPhysicaloid = new Physicaloid(this);
 
 
-/*
-        Bitmap bitmap = Bitmap.createBitmap(
-                600, // Width
-                400, // Height
-                Bitmap.Config.ARGB_8888 // Configuration
-        );
-*/
 
-/*
-        File sd = Environment.getExternalStorageDirectory();
-        File image = new File(sd+"drawable", "ic_launcher.png");
-        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
-        Bitmap bitmap = BitmapFactory.decodeFile(image.getAbsolutePath(),bmOptions);
-        bitmap = Bitmap.createScaledBitmap(bitmap,50,50,true);
-        imageView.setImageBitmap(bitmap);
+        // paint gesturepas on screen
+        Bitmap.Config conf = Bitmap.Config.ARGB_8888; // see other conf types
+        Bitmap bitmapOrg = Bitmap.createBitmap((int)w,(int) h, conf); // this creates a MUTABLE bitmap
 
-*/
-
-       // imageView.setImageResource(R.drawable.ic_action_touchpad);
-/*
-        Canvas canvas = new Canvas();
-
-        Drawable d = getResources().getDrawable(R.drawable.ic_action_touchpad);
-      //  d.setBounds(left, top, right, bottom);
-        d.draw(canvas);
-        imageView.setImageResource(canvas);
-        imageView.set
-    }*/
-
-
-       // Bitmap bitmap = Bitmap.createBitmap(100, 100, Bitmap.Config.ARGB_8888);
-
-       // Bitmap bitmap = Bitmap.createBitmap(R.drawable.ic_action_touchpad);
-
-        BitmapFactory.Options opt = new BitmapFactory.Options();
-        opt.inMutable = true;
-
-        Bitmap bitmap= BitmapFactory.decodeResource(context.getResources(),
-                R.drawable.ic_action_touchpad,opt);
-
-
-
-
-        Canvas canvas = new Canvas(bitmap);
+        Canvas canvas = new Canvas(bitmapOrg);
         Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
         paint.setColor(Color.BLACK);
-        canvas.setBitmap(bitmap);
+        canvas.setBitmap(bitmapOrg);
 
-        FloatPoint bt0 = new FloatPoint(42, 25);
-        FloatPoint bt1 = new FloatPoint(58,43);
-        FloatPoint bt2 = new FloatPoint(42,60);
-        FloatPoint bt3 = new FloatPoint(26, 43);
-
-
-
-        canvas.drawCircle(bt0.getx(), bt0.gety(), 3, paint);
-        canvas.drawCircle(bt1.getx(), bt1.gety(), 3, paint);
-        canvas.drawCircle(bt2.getx(), bt2.gety(), 3, paint);
-        canvas.drawCircle(bt3.getx(), bt3.gety(), 3, paint);
+        bt0 = new FloatPoint(w*.25f, h*.25f);
+        bt1 = new FloatPoint(w*.75f, h*.25f);
+        bt2 = new FloatPoint(w*.75f, h*.75f);
+        bt3 = new FloatPoint(w*.25f, h*.75f);
 
 
+        Paint rectPaint = new Paint();
+        rectPaint.setColor(Color.BLUE);
+        rectPaint.setStrokeWidth(10);
+        rectPaint.setStyle(Paint.Style.STROKE);
+
+        // bt0
+        canvas.drawRect(w*.0625f, w*.0625f, w * .4375f , w * .4375f, rectPaint);
+        // bt1
+        canvas.drawRect(w*.5625f, w*.0625f, w * .9375f , w * .4375f, rectPaint);
+        // bt2
+        canvas.drawRect(w*.5625f, w*.5625f, w * .9375f , w * .9375f, rectPaint);
+        // bt
+        canvas.drawRect(w*.0625f, w*.5625f, w * .4375f , w * .9375f, rectPaint);
 
 
-//        canvas.drawLine(42,25,58,43, paint);
-//        canvas.drawLine(58,43,42,60, paint);
-//       canvas.drawLine(42,60,26,43, paint);
-
-        imageView.setImageBitmap(bitmap);
+        scaledBitmap = Bitmap.createScaledBitmap(bitmapOrg,(int)w,(int)h,true);
+        Bitmap rotatedBitmap = rotateImg(scaledBitmap , 45 );
+        imageView.setImageBitmap(rotatedBitmap);
 
 }
 
+    /**
+     * Our representation of a coordinate on a bitmap
+     */
     private class FloatPoint {
         private float x;
         private float y;
@@ -163,6 +146,10 @@ public class MCUIActivity extends Activity {
 
     }
 
+    /**
+     * opens the com port and reads from arduino
+     * @param v
+     */
     public void onClickOpen(View v) {
         if(mPhysicaloid.open()) { // default 9600bps
             setEnabledUi(true);
@@ -181,12 +168,13 @@ public class MCUIActivity extends Activity {
                     try {
                         readStr = new String(buf, "UTF-8");
                     } catch (UnsupportedEncodingException e) {
-                        Log.e(TAG,e.toString());
+                        Log.e(TAG, e.toString());
                         return;
                     }
-
                     // UI thread
                     tvAppend(tvRead, readStr);
+                    // draws gesture on screen
+                    drawGesture(readStr);
                 }
             });
             //****************************************************************
@@ -194,6 +182,62 @@ public class MCUIActivity extends Activity {
         }
     }
 
+    /**
+     * Draws a gesture on the scaledBitmap
+     * @param readStr should be a string with intergers seperated by blankspace
+     */
+    public void drawGesture(String readStr){
+
+        String[] exploded= readStr.split(" ");
+        Boolean first = true;
+        String prev = "";
+        Bitmap newBmp = Bitmap.createBitmap(scaledBitmap);
+
+        for (String chr: exploded) {
+            if (chr.equals("9")) {
+                // full touch
+                newBmp = drawLine(newBmp, "0", "1");
+                newBmp = drawLine(newBmp, "1", "2");
+                newBmp = drawLine(newBmp, "2", "3");
+                newBmp = drawLine(newBmp, "3", "0");
+
+            }else if(chr.equals("\n")){
+                // removes newlines
+            }
+            else {
+                if (first) {
+                    // first input
+                    prev = chr;
+                    first = false;
+                } else {
+                    // rest of input
+                    newBmp = drawLine(newBmp, prev, chr);
+                    prev = chr;
+                }
+            }
+        }
+        // rotate and set bitmap
+        Bitmap rotatedBitmap = rotateImg(newBmp, 45);
+        imageView.setImageBitmap(rotatedBitmap);
+    }
+
+
+    /**
+     * rotates a btmap
+     * @param bmp the bitmap you want rotated
+     * @param degrees how many degrees the bitmap should be rotated
+     * @return the rotated bitmap
+     */
+    private Bitmap rotateImg(Bitmap bmp, int degrees ){
+        Matrix matrix = new Matrix();
+        matrix.postRotate(degrees);
+        return Bitmap.createBitmap(bmp , 0, 0, bmp.getWidth(), bmp.getHeight(), matrix, true);
+    }
+
+    /**
+     * called when user clicks close. Closes the reading from comport(arduino)
+     * @param v
+     */
     public void onClickClose(View v) {
         if(mPhysicaloid.close()) {
             setEnabledUi(false);
@@ -212,10 +256,55 @@ public class MCUIActivity extends Activity {
  //       }
  //   }
 
+    /**
+     * called when user clicks clear. Clears the output textview.
+     * @param v
+     */
     public void onClickClear(View v) {
+        drawGesture("0 1 2 3 0");
         tvRead.setText("");
     }
 
+    /**
+     * Draws a line on a bitmap from one point to another
+     * @param bmp the bitmap to be drawn on
+     * @param bt_s source point
+     * @param bt_d destination poinr
+     * @return returns the new bitmap with the line on
+     */
+    private Bitmap drawLine(Bitmap bmp , String bt_s, String bt_d ){
+        FloatPoint bt_s_point =  stringToFloatpoint(bt_s);
+        FloatPoint bt_d_point =  stringToFloatpoint(bt_d);
+
+        Canvas canvas = new Canvas(bmp);
+        Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        paint.setColor(Color.BLACK);
+        paint.setStrokeWidth(7f);
+        canvas.setBitmap(bmp);
+
+        canvas.drawLine(bt_s_point.getx(), bt_s_point.gety(), bt_d_point.getx(), bt_d_point.gety(), paint);
+        return bmp;
+    }
+
+    /**
+     * converts a string char to a FloatPoint( a (x,y) coordinate)
+     * @param str should be the number of one of our input buttons 0-3
+     * @return the corresponding buttons coordinates
+     */
+    private FloatPoint stringToFloatpoint(String str){
+        switch (Integer.parseInt(str)){
+            case 0:
+                return bt0;
+            case 1:
+                return bt1;
+            case 2:
+                return bt2;
+            case 3:
+                return bt3;
+            default:
+                return bt0;
+        }
+    }
 
 
     //****************************************************************
@@ -230,7 +319,9 @@ public class MCUIActivity extends Activity {
     // onPreUpload  |
     // onUploading  | -> onError
     // onPostUpload |
-
+    /**
+     * uploads program from android phone to arduino
+     */
     private UploadCallBack mUploadCallback = new UploadCallBack() {
         @Override
         public void onPreUpload() {
@@ -293,40 +384,15 @@ public class MCUIActivity extends Activity {
         if(on) {
             btOpen.setEnabled(false);
             btClose.setEnabled(true);
-         //   btWrite.setEnabled(true);
             btUpload.setEnabled(true);
-       //     etWrite.setEnabled(true);
             tvRead.setEnabled(true);
         } else {
             btOpen.setEnabled(true);
             btClose.setEnabled(false);
-      //      btWrite.setEnabled(false);
             btUpload.setEnabled(true);
-       //     etWrite.setEnabled(false);
             tvRead.setEnabled(false);
         }
     }
 
 
-    public static Bitmap drawableToBitmap (Drawable drawable) {
-        Bitmap bitmap = null;
-
-        if (drawable instanceof BitmapDrawable) {
-            BitmapDrawable bitmapDrawable = (BitmapDrawable) drawable;
-            if(bitmapDrawable.getBitmap() != null) {
-                return bitmapDrawable.getBitmap();
-            }
-        }
-
-        if(drawable.getIntrinsicWidth() <= 0 || drawable.getIntrinsicHeight() <= 0) {
-            bitmap = Bitmap.createBitmap(1, 1, Bitmap.Config.ARGB_8888); // Single color bitmap will be created of 1x1 pixel
-        } else {
-            bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
-        }
-
-        Canvas canvas = new Canvas(bitmap);
-        drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
-        drawable.draw(canvas);
-        return bitmap;
-    }
 }
